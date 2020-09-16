cd27c45eba60b1bdf3cf65204450795e137338d4
Joseph Kim <jk851227@gmail.com>
2020-09-16 10:40:04 -0700

    Configured get players method

--- server/controllers/player.controller.js
+++ server/controllers/player.controller.js
@@ -6,5 +6,28 @@ module.exports = {
         Player.create(req.body)
             .then(data => res.json(data))
             .catch(err => res.json(err))
+    },
+    // Get Players & Player
+    allPlayers: (req, res) => {
+        Player.find({})
+            .then(data => {
+                if(data.length > 0) {
+                    res.json(data)
+                } else {
+                    res.status(500).json({ error: "There are no players" })
+                }
+            })
+            .catch(err => res.json(err))
+    },
+    onePlayer: (req, res) => {
+        Player.findOne({ _id: req.params.id })
+            .then(data => res.json(data))
+            .catch(err => {
+                if(err.kind == "ObjectId") {
+                    res.json({ message: "There is no player with that ID" })
+                } else {
+                    res.json(err)
+                }
+            })
     }
 }
--- server/routes/player.routes.js
+++ server/routes/player.routes.js
@@ -3,4 +3,7 @@ const playerController = require('../controllers/player.controller');
 module.exports = app => {
     // C
     app.post("/api/players", playerController.createPlayer);
+    // R
+    app.get("/api/players", playerController.allPlayers);
+    app.get("/api/players/:id", playerController.onePlayer);
 }
